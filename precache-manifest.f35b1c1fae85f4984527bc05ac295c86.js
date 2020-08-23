ecd50876af685117a081ccb463af6a62de516a96
prasannabalaji7 <prasannabalaji07@gmail.com>
2020-08-23 18:30:56 +0800

    Updates

---
+++ precache-manifest.f35b1c1fae85f4984527bc05ac295c86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db947043a2ec2fb27d17b761b2219fc",
+    "url": "/sentimental-widget/index.html"
+  },
+  {
+    "revision": "9d0b539264581333a2f0",
+    "url": "/sentimental-widget/static/css/main.ee308257.chunk.css"
+  },
+  {
+    "revision": "55cffe658882c5287fcc",
+    "url": "/sentimental-widget/static/js/2.677785fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sentimental-widget/static/js/2.677785fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0b539264581333a2f0",
+    "url": "/sentimental-widget/static/js/main.e4546ad5.chunk.js"
+  },
+  {
+    "revision": "d7fda0f21399ca4be217",
+    "url": "/sentimental-widget/static/js/runtime-main.a7c01257.js"
+  },
+  {
+    "revision": "1306e00542acbf429e0c69fdd29f90ba",
+    "url": "/sentimental-widget/static/media/smile.1306e005.svg"
+  }
+]);
